29aa4908ad5756acb07ed378b13a0178ab9a8636
disasterdistress <44525752+disasterdistress@users.noreply.github.com>
2019-09-02 15:22:34 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,137 @@
+// CODE EXPLAINED channel
+
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+
+let LIST, id;
+
+// get item from local storage
+let data = localStorage.getItem("TODO");
+if(data)
+{
+	LIST=JSON.parse(data);
+	id = LIST.length;
+	loadList(LIST);
+}else{
+    LIST=[];
+    id=0;
+}
+
+function loadList(array)
+{
+	array.forEach(function(item){
+		addToDo(item.name,item.id,item.done,item.trash);
+	});
+}
+
+
+//clear
+clear.addEventListener("click",function(){
+	localStorage.clear();
+	location.reload();
+});
+
+// add item to local storage
+localStorage.setItem("TODO", JSON.stringify(LIST));
+
+const options={weekday:"long",month:"short",day:"numeric"};
+const today=new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US",options);
+
+
+// add an item
+
+function addToDo(toDo, id, done, trash)
+{
+     if(trash){return;}
+
+     
+     const DONE = done ? CHECK : UNCHECK;
+     const LINE = done ? LINE_THROUGH : "";
+	const item = `  <li class="item">
+				<i class="fa ${DONE} co" job="complete" id="${id}"></i>
+				<p class="text ${LINE}">${toDo}</p>
+				<i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+	             </li>
+	                `;
+const position = "beforeend";
+
+list.insertAdjacentHTML(position,item);
+
+
+}
+
+// using enter key
+
+document.addEventListener("keyup",function(event)
+{
+	if(event.keyCode == 13)
+	{
+		const toDo = input.value;
+
+		if(toDo)
+		{
+			addToDo(toDo, id, false ,false);
+
+			LIST.push({
+				name : toDo,
+				id : id,
+				done : false,
+				trash : false
+			});
+			localStorage.setItem("TODO", JSON.stringify(LIST));
+			id++;
+		}
+		input.value = "";
+	}
+});
+
+
+//addToDo("codde", 1, false, false);
+
+//complete
+
+function completeToDo(element)
+{
+	element.classList.toggle(CHECK);
+	element.classList.toggle(UNCHECK);
+	element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+	LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+
+
+// remove todo
+
+function removeToDo(element){
+	element.parentNode.parentNode.removeChild(element.parentNode);
+
+	LIST[element.id].trash = true;
+
+}
+
+
+//target the items created dynamically
+
+list.addEventListener("click", function(event){
+const element = event.target;
+const elementJob = element.attributes.job.value;
+if(elementJob == "complete")
+	{ 
+		completeToDo(element);
+
+	} else if(elementJob == "delete"){
+          removeToDo(element);
+	}
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
